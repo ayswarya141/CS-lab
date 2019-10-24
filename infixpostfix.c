7fd3b4b474d9343a0f96c4e111bf11f301b9ac12
joe8101 <47602421+joe8101@users.noreply.github.com>
2019-10-24 05:45:08 +0530

    Update infixpostfix.c

--- infixpostfix.c
+++ infixpostfix.c
@@ -1,111 +1,224 @@
-#include <stdio.h>
-#define size 20
-char postfix[size],stack[size];
-int k=-1;
-int isoperator(char t){
-    if( t=='^' || t=='/' ||  t=='*' || t=='+' || t=='-'){
-        return 1;
+#include<stdio.h>
+#include<ctype.h>
+#include<stdlib.h>
+typedef struct node{
+  char character;
+  struct node *link;
+}node;
+typedef struct eval{
+  int number;
+  struct eval *link;
+}eval;
+node *top=NULL;
+node *front=NULL,*rear=NULL;
+eval *start=NULL;
+char infix[50];
+void convert(){
+  printf("\n");
+  for (int i=0;infix[i]!='\0';i++){
+    if (isalpha(infix[i])){
+        node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+	if (front==NULL){
+	  front=rear=newnode;
+	}
+	else{
+	  rear->link=newnode;
+	  rear=newnode;
+	}
+	newnode->character=infix[i];
+    }
+    else if (infix[i]=='('){
+        node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+	if (top==NULL){
+	  top=newnode;
+	}
+	else{
+	  newnode->link=top;
+	  top=newnode;
+	}
+	newnode->character=infix[i];
+    }
+    else if (infix[i]==')'){
+      while (top->character!='('){
+	node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+	if (front==NULL){
+	  front=rear=newnode;
+	}
+	else{
+	  rear->link=newnode;
+	  rear=newnode;
+	}
+	newnode->character=top->character;
+	top=top->link;
+      }
+      top=top->link;
+    }
+    else if (infix[i]=='+'||infix[i]=='-'){
+      int flag=0;
+      if (top==NULL)flag=1;
+      while(flag==0){
+	if (top->character=='+'||top->character=='-'||top->character=='*'||top->character=='/'||top->character=='^'){
+	  node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+	  newnode->character=top->character;
+	  if (front==NULL){
+	    front=rear=newnode;
+	  }
+	  else{
+	    rear->link=newnode;
+	    rear=newnode;
+	  }
+	  top=top->link;
+	  if (top==NULL)flag=1;
+	}
+	else
+	  flag=1;
+      }
+      node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+      newnode->character=infix[i];
+      if(top==NULL){
+	top=newnode;
+      }
+      else{
+	newnode->link=top;
+	top=newnode;
+      }
+    }
+    else if(infix[i]=='*'||infix[i]=='/'){
+      int flag=0;
+      if (top==NULL)flag=1;
+      while(flag==0){
+	if (top->character=='*'||top->character=='/'||top->character=='^'){
+	  node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+	  newnode->character=top->character;
+	  if (front==NULL){
+	    front=rear=newnode;
+	  }
+	  else{
+	    rear->link=newnode;
+	    rear=newnode;
+	  }
+	  top=top->link;
+	  if (top==NULL)flag=1;
+	}
+	else
+	  flag=1;
+      }
+      node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+      newnode->character=infix[i];
+      if(top==NULL){
+	top=newnode;
+      }
+      else{
+	newnode->link=top;
+	top=newnode;
+      }
+    }
+    else if(infix[i]=='^'){
+      int flag=0;
+      if (top==NULL)flag=1;
+      while(flag==0){
+	if (top->character=='^'){
+	  node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+	  newnode->character=top->character;
+	  if (front==NULL){
+	    front=rear=newnode;
+	  }
+	  else{
+	    rear->link=newnode;
+	    rear=newnode;
+	  }
+	  top=top->link;
+	  if (top==NULL)flag=1;
+	}
+	else
+	  flag=1;
+      }
+      node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+      newnode->character=infix[i];
+      if(top==NULL){
+	top=newnode;
+      }
+      else{
+	newnode->link=top;
+	top=newnode;
+      }
     }
     else{
-        return 0;
+      printf("Invalid Expression\n");
+      exit(0);
     }
+  }
+  while(top!=NULL){
+    node *newnode; newnode=(node*)malloc(sizeof(node)); newnode->link=NULL;
+    newnode->character=top->character;
+    rear->link=newnode;
+    rear=newnode;
+    top=top->link;
+  }
 }
-void push(char elem){
-    stack[++k]=elem;
-}
-char pop(){
-    if(k==-1){
-        return EOF;
+void evl(){
+  printf("\n");
+  node *t;
+  int a,b,r;
+  for (t=front;t!=NULL;t=t->link){
+    if (isalpha(t->character)){ 
+      eval *newnode; newnode=(eval*)malloc(sizeof(eval)); newnode->link=NULL;
+      printf("Enter value of %c: ",t->character);
+      scanf("%d",&newnode->number);
+      if (start==NULL)
+	start=newnode;
+      else{
+	newnode->link=start;
+	start=newnode;
+      }
     }
-    else{
-        return stack[k--];
+    else if (t->character=='+'||t->character=='-'||t->character=='*'||t->character=='/'||t->character=='^'){
+      a=start->number;
+      start=start->link;
+      b=start->number;
+      start=start->link;
+      if (t->character=='+')
+	r=a+b;
+      else if(t->character=='-'){
+	r=b-a;
+      }
+      else if(t->character=='*'){
+	r=b*a;
+      }
+      else if(t->character=='/'){
+	r=b/a;
+      }
+      else{
+	r=1;
+	for(int i=0;i<a;i++)
+	  r=r*b;
+      }
+      eval *newnode; newnode=(eval*)malloc(sizeof(eval)); newnode->link=NULL;
+      newnode->number=r;
+      if (start==NULL)
+	start=newnode;
+      else{
+        newnode->link=start;
+	start=newnode;
+      }
     }
-}
-int precedence(char t){
-    char st=pop();
-    push(st);
-    if(st==t){
-        return 0;
-    }
-    else if(t=='^'){
-        return 0;
-    }
-    else if(t=='/' || t=='*'){
-        if(st=='+' || st=='-'){
-            return 0;
-        }
-        else{
-            return 1;
-        }
-    }
-    else if(t=='+' || t=='-'){
-        return 1;
-    }
-    else{
-        return 0;
-    }
-}
-void infix_to_postfix(char infix[]){
-    int i=0,j=0;
-    char x;
-    push('(');
-    while((infix[i])!='\0'){
-        char term=infix[i];
-        if(isoperator(term)==1){
-            if(precedence(term)==1){
-                while((x=pop())!='('){
-                    postfix[j++]=x;
-                }
-                push(x);
-                push(term);
-            }
-            else{
-                push(term);
-            }
-        }
-        else if( term=='('){
-            push(term);
-        }
-        else if(term==')'){
-            while((x=pop())!='('){
-                postfix[j++]=x;
-            }
-        }
-        else{
-            postfix[j++]=term;
-        }
-        i++;
-    }
-    while((x=pop())!='('){
-        postfix[j++]=x;
-    }
-    postfix[j]='\0';
-    int p=0;
-    while(postfix[p]!='\0'){
-        printf("%c",postfix[p++]);
-    }
+    printf("%c| ",t->character);
+    for(eval *print=start;print!=NULL;print=print->link)
+      printf("%d ",print->number);
+    printf("\n");
+  }
 }
 int main(){
-    int ch;
-    while(1){
-        printf("\nEnter your choice:\n1.Infix to Postfix conversion 2.Evaluation of expression 3.Exit\n");
-        scanf("%d",&ch);
-        if(ch==1){
-            char infix[size];
-            printf("Enter the infix expression:\n");
-            scanf("%s",infix);
-            char arr[size];
-            infix_to_postfix(infix);
-        }
-        /*else if(ch==2){
-            printf("Press 1 if you want to give another postfix expression,otherwise press any other\n");
-            char ch=getchar();
-            if(ch==1){
-                printf("Enter a postfix expression:\n");
-            }
-        }*/
-        else{
-            return 0;
-        }
-    }
+  printf("Enter expression: ");
+  scanf("%s",infix);
+  convert();
+  node *t;
+  printf("Postfix: ");
+  for (t=front;t!=NULL;t=t->link)
+    printf("%c",t->character);
+  printf("\n");
+  evl();
+  printf("\nResult: %d",start->number);
+  printf("\n");
+  return 0;
 }
